7de886981236f417b8b883b3877b941a119492e8
Mithun636 <mithun270603@gmail.com>
2024-08-22 17:10:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+  const [fruits] = useState([
+    { id: 1, name: 'Apple', Kg:'(1KG)',price: 110 },
+    { id: 2, name: 'Orange', Kg:'(1KG)', price: 75},
+    { id: 3, name: 'Cherry',Kg:'(1KG)', price: 80 },
+  ]);
+
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (fruit) => {
+    setCartItems([...cartItems, fruit]);
+  };
+
+  const total = cartItems.reduce((sum, item) => sum + item.price, 0);
+
+  return (
+    <div style={{ padding: '20px' }}>
+      
+    <h1>Fruit Shop</h1>
+      <div class="items">
+      <h2>Available Fruits</h2><hr></hr>
+      <ul>
+        {fruits.map((fruit) => (
+          <li key={fruit.id}>
+            {fruit.name} {fruit.Kg}- ₹{fruit.price.toFixed(2)}
+            <button onClick={() => addToCart(fruit)} style={{ marginLeft: '10px' }}>
+              Add to Cart
+            </button>
+          </li>
+        ))}
+      </ul>
+      </div>
+      <div class='cart'>
+      <h2>Your Cart</h2><hr></hr>
+      {cartItems.length === 0 ? (
+        <p>Your cart is empty.</p>
+      ) : (
+        <div>
+          <ul>
+            {cartItems.map((item, index) => (
+              <li key={index}>
+                {item.name} - ₹{item.price.toFixed(2)}
+              </li>
+            ))}
+          </ul>
+          <h3>Total: ₹{total.toFixed(2)}</h3>
+        </div>
+      )}
+    </div>
+    </div>
+  );
+};
+
+export default App;
+
